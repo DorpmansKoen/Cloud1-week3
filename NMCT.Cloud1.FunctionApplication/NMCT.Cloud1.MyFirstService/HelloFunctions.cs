8623b9bf63c21cededa1299924eca47f3aa6cc30
Koen <koen.dorpmans@student.howest.be>
2017-10-13 10:11:17 +0200

    Azure HTTP GET Functions

--- NMCT.Cloud1.FunctionApplication/NMCT.Cloud1.MyFirstService/HelloFunctions.cs
+++ NMCT.Cloud1.FunctionApplication/NMCT.Cloud1.MyFirstService/HelloFunctions.cs
@@ -12,6 +12,7 @@ namespace NMCT.Cloud1.MyFirstService
 {
     public static class HelloFunctions
     {
+        #region First Function
         [FunctionName("HelloFunctions")]
         public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "HttpTriggerCSharp/name/{name}")]HttpRequestMessage req, string name, TraceWriter log)
         {
@@ -20,9 +21,62 @@ namespace NMCT.Cloud1.MyFirstService
             // Fetching the name from the path parameter in the request URL
             return req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
         }
+        #endregion
 
+        #region Test Function
+        [FunctionName("TestParameters")]
+        public static HttpResponseMessage TestParameters([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "TestParameters/{name}")]HttpRequestMessage req, string name, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
 
+            // Fetching the name from the path parameter in the request URL
+            return req.CreateResponse(HttpStatusCode.OK, "Hello Again" + name);
+        }
+        #endregion
 
+        #region Rekenen
+        [FunctionName("Sum")]
+        public static HttpResponseMessage Sum([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "rekenmachine/som/{getal1}/{getal2}")]HttpRequestMessage req, int getal1, int getal2, TraceWriter log)
+        {
+            int som = getal1 + getal2;
+            return req.CreateResponse(HttpStatusCode.OK, "Som is " + som);
+        }
+
+        [FunctionName("Divide")]
+        public static HttpResponseMessage Divide([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "rekenmachine/devide/{getal1}/{getal2}")]HttpRequestMessage req, int getal1, int getal2, TraceWriter log)
+        {            
+            if (getal2 != 0)
+            {
+                int quotient = getal1 / getal2;
+                return req.CreateResponse(HttpStatusCode.OK, "Quotient is " + quotient);
+            } else
+            {
+                return req.CreateResponse(HttpStatusCode.Forbidden);
+            }
+            
+        }
+        #endregion
+
+        [FunctionName("History")]
+        public static HttpResponseMessage History([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "history")]HttpRequestMessage req, TraceWriter log)
+        {
+            string from = string.Empty;
+            string to = string.Empty;
+
+            foreach (var param in req.GetQueryNameValuePairs())
+            {
+                if (param.Key.Equals("from"))
+                {
+                    from = param.Value;
+                }
+                else
+                {
+                    to = param.Value;
+                }
+            }
+            string value = $"From {from} To {to}";
+            return req.CreateResponse(HttpStatusCode.OK, value);
+        }
 
 
         [FunctionName("AddRegistration")]
